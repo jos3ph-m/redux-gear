5ccbc3663994e8f56b26213d93f4ffa02f76036c
Joseph M <jmedina1985@hotmail.com>
2021-08-12 14:26:56 -0700

    Getting started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React from 'react';
 // components
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 // items
-import cartItems from "./cart-items";
+import cartItems from './cart-items';
 // redux stuff
+
+// store - stores data, think of state
+// reducer - function that used to update store
 
 function App() {
   // cart setup
