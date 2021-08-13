bdbe0ef2c13e381549b9e666e5d190095060d2ca
Joseph M <jmedina1985@hotmail.com>
2021-08-13 13:56:54 -0700

    More actions

--- src/App.js
+++ src/App.js
@@ -36,6 +36,9 @@ function reducer(state, action) {
   if (action.type === 'INCREASE') {
     return { count: state.count + 1 };
   }
+  if (action.type === 'RESET') {
+    return { count: 0 };
+  }
   return state;
 }
 
