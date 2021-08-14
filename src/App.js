81c6893d1fa96010a22c8e79519ac222b343ac37
Joseph M <jmedina1985@hotmail.com>
2021-08-14 15:29:06 -0700

    More complicated state

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function reducer(state, action) {
   if (action.type === 'DECREASE') {
     // DONT MUTATE
     // state.count = state.count - 1;
-    return { count: state.count - 1 };
+    return { ...state, count: state.count - 1 };
   }
   if (action.type === 'INCREASE') {
     return { count: state.count + 1 };
