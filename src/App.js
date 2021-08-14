90e5d754dda46bc60f4b9d1a001c252682d44a5a
Joseph M <jmedina1985@hotmail.com>
2021-08-14 15:35:53 -0700

    More complicated state

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ const initialStore = {
 const DECREASE = 'DECREASE';
 const INCREASE = 'INCREASE';
 const RESET = 'RESET';
+const CHANGE_NAME = 'CHANGE_NAME';
 
 //reducer
 function reducer(state, action) {
@@ -44,7 +45,7 @@ function reducer(state, action) {
   if (action.type === RESET) {
     return { ...state, count: 0 };
   }
-  if (action.type === 'CHANGE_NAME') {
+  if (action.type === CHANGE_NAME) {
     return { ...state, name: 'audrey' };
   }
   return state;
@@ -53,7 +54,7 @@ function reducer(state, action) {
 // store
 const store = createStore(reducer, initialStore);
 store.dispatch({ type: DECREASE });
-store.dispatch({ type: 'CHANGE_NAME' });
+store.dispatch({ type: CHANGE_NAME });
 store.dispatch({ type: RESET });
 store.dispatch({ type: INCREASE });
 store.dispatch({ type: INCREASE });
