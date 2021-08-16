0e75a2c0bfd95f58eb52a01e5278d5e6fbe39666
Joseph M <jmedina1985@hotmail.com>
2021-08-15 20:39:37 -0700

    More complicated state

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,5 @@
 export const DECREASE = 'DECREASE';
 export const INCREASE = 'INCREASE';
 export const REMOVE = 'REMOVE';
-export const RESET = 'RESET';
-export const CHANGE_NAME = 'CHANGE_NAME';
+export const CLEAR_CART = 'CLEAR_CART';
+export const GET_TOTAL = 'GET_TOTAL';
