9c767a2824164393e7a83281ba0dd75e8d2b6ec7
Joseph M <jmedina1985@hotmail.com>
2021-08-16 14:39:20 -0700

    Doing some refactoring

--- src/reducer.js
+++ src/reducer.js
@@ -1,16 +1,6 @@
 import { DECREASE, INCREASE } from './actions';
 
 function reducer(state, action) {
-  console.log({ state, action });
-  if (action.type === DECREASE) {
-    // DONT MUTATE
-    // state.count = state.count - 1;
-    return { ...state, count: state.count - 1 };
-  }
-  if (action.type === INCREASE) {
-    return { ...state, count: state.count + 1 };
-  }
-
   return state;
 }
 
