4662256c8eb4fe876caef5bcb6895a300dd64aaa
Joseph M <jmedina1985@hotmail.com>
2021-08-17 14:37:37 -0700

    Provider setup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 const Navbar = () => {
   return (
     <nav>
@@ -17,4 +18,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default connect()(Navbar);
