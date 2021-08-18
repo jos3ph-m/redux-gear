987bf961d9449775f3293249170538673d057f02
Joseph M <jmedina1985@hotmail.com>
2021-08-18 16:54:59 -0700

    dispatch clear cart

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CartItem from './CartItem';
 import { connect } from 'react-redux';
+import { CLEAR_CART } from '../actions';
 const CartContainer = ({ cart = [], total }) => {
   if (cart.length === 0) {
     return (
