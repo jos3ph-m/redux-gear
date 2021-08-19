059ed2c5a05ffe3a0f15af44887515e57077315f
Joseph M <jmedina1985@hotmail.com>
2021-08-19 15:07:22 -0700

    REMOVE action setup

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { INCREASE, DECREASE, REMOVE } from '../actions';
 
 const CartItem = ({ img, title, price, amount }) => {
   return (
