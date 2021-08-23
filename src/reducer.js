c8810efa2a3c7179346561630062ac26323a036c
Joseph M <jmedina1985@hotmail.com>
2021-08-23 14:46:48 -0700

    GET_TOTALS action

--- src/reducer.js
+++ src/reducer.js
@@ -38,6 +38,9 @@ function reducer(state, action) {
     let { total, amount } = state.cart.reduce(
       (cartTotal, cartItem) => {
         const { price, amount } = cartItem;
+        const itemTotal = price * amount;
+
+        cartTotal.total += itemTotal;
         cartTotal.amount += amount;
         return cartTotal;
       },
