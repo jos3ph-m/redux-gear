568535714ce95b785f8f7784b1c33b1c1fb7db29
Joseph M <jmedina1985@hotmail.com>
2021-08-25 14:16:15 -0700

    Reducer default state

--- src/actions.js
+++ src/actions.js
@@ -5,4 +5,6 @@ export const CLEAR_CART = 'CLEAR_CART';
 export const GET_TOTALS = 'GET_TOTALS';
 export const TOGGLE_AMOUNT = 'TOGGLE_AMOUNT';
 
-export const removeItem = () => {};
+export const removeItem = () => {
+  return { type: REMOVE, payload: { id } };
+};
